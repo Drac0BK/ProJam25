00f6d91e41ccca2dbe6f0a171b45f06d152709c0
Madcrafty <rantao877@gmail.com>
2025-01-29 02:21:01 +1100

    Added StateMachine to Sliders (sliding spiders) so they walk around and hunt the player

---
+++ ProJam/Assets/Scripts/AI/IncarnetScripts/States/IncarnateAI/PursuitState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// Moves the agent toward it's target
+public class PursuitState : OverworldState
+{
+    public Transform target;
+    public Vector3 oldTarget;
+    public PursuitState(OverworldStateSystem system) : base(system)
+    {
+    }
+    public override IEnumerator Start()
+    {
+        target = _system.fov.GetTargetFromTag("Player");
+        _system.target = target;
+        if (target == null)
+        {
+            Debug.LogError("Something really dungooffed");
+        }
+        oldTarget = target.position;
+        _system.fov.viewRadius = _system.targetedSightDistance;
+        yield break;
+    }
+    public override IEnumerator Move()
+    {
+        //do xyz
+        target = _system.fov.GetTargetFromTag("Player");
+        if (target != null)
+        {
+            oldTarget = target.position;
+            _system.nav.SetDestination(target.position);
+        }
+        else
+        {
+            _system.nav.SetDestination(oldTarget);
+            if (_system.nav.remainingDistance<0.1f)
+            {
+                _system.target = null;
+                _system.SetState(new WanderState(_system));
+            }
+        }
+        
+        yield break;
+    }
+}
