e8521fa3da987d569effd24e0a0362e89d5aa976
Drac0BK <baik2704@gmail.com>
2025-01-30 21:03:36 +1100

    new images
    for the project phishing and trojan

--- ProJam/Assets/User0/MyPlayer.cs
+++ ProJam/Assets/User0/MyPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,7 @@ public class MyPlayer : MonoBehaviour
     public bool normal = false;
     bool isInteracting = false;
     // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
